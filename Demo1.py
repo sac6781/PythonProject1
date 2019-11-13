d23c0d274639dd115061c407d78554c97e26be3d
sac6781 <sachin.singh070188@gmail.com>
2019-11-13 17:48:16 +0530

    New Query Added

--- Demo1.py
+++ Demo1.py
@@ -13,3 +13,9 @@ mycursor.execute("select * from sach where name1 in('Sachin','Singh') and id<500
 result=mycursor.fetchall()
 for j in result:
     print(j)
+
+print("Print name values where id is in between 3000 to 100000")
+mycursor.execute("Select name1 from sach where id>3000 ")
+result2=mycursor.fetchall()
+for k in result2:
+    print(k)
